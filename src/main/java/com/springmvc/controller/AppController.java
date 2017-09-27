44c69c19ef68f3bfb1adbace7f958ad1af971040
QingQuan <thinlh@gmail.com>
2017-09-27 11:22:57 +0700

    init

---
+++ src/main/java/com/springmvc/controller/AppController.java
@@ -0,0 +1,18 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class AppController {
+
+	 @RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	    public String listEmployees(ModelMap model) {
+		 	model.addAttribute("mess", "Thin Lau");
+	        
+	        return "index";
+	    }
+}
